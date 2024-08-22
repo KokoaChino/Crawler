a685374e2c1170010ab0f4f8c448268a8e2c6012
Kokoa_Chino <2178740980@qq.com>
2024-08-22 19:52:56 +0800

    [1.0] Initial

---
+++ src/main/java/com/book/A.java
@@ -0,0 +1,39 @@
+package com.book;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class A {
+
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://jufei66.com/?cases_90/#&gid=1&pid=1")).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String html = response.body();
+            Matcher matcher = Pattern.compile("src=\"(.*?jpg)\"").matcher(html);
+            for (int i = 0; matcher.find(); i++) {
+                HttpRequest imageRequest = HttpRequest.newBuilder().uri(new URI("http://jufei66.com" + matcher.group(1))).build();
+                HttpResponse<InputStream> imageResponse = client.send(imageRequest, HttpResponse.BodyHandlers.ofInputStream());
+                InputStream imageInput = imageResponse.body();
+                FileOutputStream stream = new FileOutputStream(String.format("千恋万花 %d.jpg", i));
+                try (stream; imageInput) {
+                    int size;
+                    byte[] data = new byte[1024];
+                    while ((size = imageInput.read(data)) > 0) {
+                        stream.write(data, 0, size);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/book/B.java
@@ -0,0 +1,38 @@
+package com.book;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class B {
+
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            for (int i = 1; i <= 12; i++) {
+                File file = new File("Output\\" + i);
+                if (!file.exists()) file.mkdirs();
+                for (int j = 1; j <= 6; j++) {
+                    HttpRequest imageRequest = HttpRequest.newBuilder().uri(new URI("https://hoshitele-anime.com/dist/img/story/ep" + i + "/img0" + j +".jpg?ver=2.086")).build();
+                    HttpResponse<InputStream> imageResponse = client.send(imageRequest, HttpResponse.BodyHandlers.ofInputStream());
+                    InputStream imageInput = imageResponse.body();
+                    FileOutputStream stream = new FileOutputStream(file.getPath() + "\\" + j + ".webp");
+                    try (stream; imageInput) {
+                        int size;
+                        byte[] data = new byte[2048];
+                        while ((size = imageInput.read(data)) > 0) {
+                            stream.write(data, 0, size);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
